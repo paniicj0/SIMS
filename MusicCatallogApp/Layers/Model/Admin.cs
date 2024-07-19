7cad2994fbab2649ac4c655946f83a08dc94e59e
Jovana Panic <panic.sv64.2022@uns.ac.rs>
2024-07-19 20:02:29 +0200

    Added model for classes

---
+++ MusicCatallogApp/Layers/Model/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCatallogApp.Layers.Model
+{
+    class Admin: User
+    {
+        public Admin(int id, string name, string surname, string email, string password, List<string> favourites, bool showReviews, bool showConcact, bool blocked)
+            : base(id,name, surname, email, password, favourites, showReviews, showConcact, blocked)
+        {}
+    }
+}
