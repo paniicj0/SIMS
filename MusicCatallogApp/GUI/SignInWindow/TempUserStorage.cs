2e4fd81793d07077b82afcf61e723c3a47093d1c
Jovana Panic <panic.sv64.2022@uns.ac.rs>
2024-08-09 00:48:44 +0200

    Added verification mail for user

---
+++ MusicCatallogApp/GUI/SignInWindow/TempUserStorage.cs
@@ -0,0 +1,31 @@
+﻿using MusicCatallogApp.Layers.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCatallogApp.GUI.SignInWindow
+{
+    public static class TempUserStorage
+    {
+        private static Dictionary<string, User> tempUsers = new Dictionary<string, User>();
+
+        public static void AddUser(string code, User user)
+        {
+            tempUsers[code] = user;
+        }
+
+        public static User GetUser(string code)
+        {
+            if (tempUsers.ContainsKey(code))
+            {
+                var user = tempUsers[code];
+                tempUsers.Remove(code);
+                return user;
+            }
+            return null;
+        }
+    }
+
+}
